6a20c5c69c8ddcf40c27f77c84b74f38504c88b8
Morgan Kenyon <morganwkenyon@gmail.com>
2022-09-10 14:21:51 -0500

    Working on digital inputs example program

--- src/juniper/compileFile.ps1
+++ src/juniper/compileFile.ps1
@@ -1,3 +1,3 @@
 ## Compiles juniper file, outputs a main.cpp to ../compiled folder
 
-Juniper.exe -s fade.jun -o ../compiled/src/main.cpp
+Juniper.exe -s digitalInputs.jun -o ../compiled/src/main.cpp
